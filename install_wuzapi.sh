d37073ca2dc8b8faeb7484a31b3a8332660054a7
Andredye_Tasker <andredye1221@gmail.com>
2024-11-15 02:47:41 -0300

    Update install_wuzapi.sh

--- install_wuzapi.sh
+++ install_wuzapi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "##### ESTE PROCESO TARDARA ENTRE 15 A 20 MINUTOS #####"
+echo "##### ESTE PROCESSO TARDARÁ ENTRE 15 A 20 MINUTOS #####"
+echo " Criado por Andredye desenvolvedor Tasker"
 
 # Instalar Git e Go
 echo "Instalando Git e Go..."
@@ -32,10 +33,19 @@ else
     exit 1
 fi
 
-# Concedendo permissões para aplicativos externos no Termux
-mkdir -p ~/.termux && echo "allow-external-apps=true" >> ~/.termux/termux.properties
+# Concedendo permissão para aplicativos externos no Termux
+mkdir -p ~/.termux && echo "allow-external-apps=true" > ~/.termux/termux.properties
 termux-reload-settings
 echo "Permissão para aplicativos externos configurada."
+
+# Adicionando a reconfiguração automática de permissão no .bashrc
+if ! grep -q "allow-external-apps=true" ~/.bashrc; then
+    echo -e "\n# Configuração automática para permitir aplicativos externos no Termux após reinício" >> ~/.bashrc
+    echo "mkdir -p ~/.termux && echo 'allow-external-apps=true' > ~/.termux/termux.properties && termux-reload-settings" >> ~/.bashrc
+    echo "Configuração para reativação automática adicionada ao .bashrc"
+else
+    echo "Configuração para reativação automática já existente no .bashrc"
+fi
 
 # Executar WuzAPI
 echo "Executando WuzAPI..."
